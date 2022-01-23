34ccde68129224d4a3deec89d913ac15ad087363
kekusgamestudio <kekuseg@gmail.com>
2022-01-23 18:45:17 -0300

    Saqué algunos espacios

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -13,8 +13,6 @@ export const GifExpertApp = () => {
             <h2>GifExpertApp</h2>
             <AddCategory setCategories={setCategories} />
             <hr />
-
-
             <ol>
                 {
                     categories.map(category =>
